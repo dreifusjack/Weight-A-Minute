b83997d6a35af4c6c86ee4fb91cc3577523650f4
dreifusjack <dreifus.j@northeastern.edu>
2025-04-08 18:22:00 -0400

    implmentation of deleting a gym endpoint

--- api/backend/gyms/gyms_routes.py
+++ api/backend/gyms/gyms_routes.py
@@ -8,7 +8,29 @@ gyms = Blueprint('gyms', __name__)
 
 # routes go here
 
-# route to get all gym requests 
+# Remove the gym with the given id from the db
+@gyms.route('/gyms/<gymId>', methods=['DELETE'])
+def remove_gym(gymId):
+  try:
+    query = '''
+    DELETE FROM Gyms WHERE gymId = %s
+    '''
+
+    cursor = db.get_db().get_cursor()
+    cursor.execute(query, (gymId,))
+
+    if cursor.rowcount == 0: 
+      return jsonify({'message': f'Gym with id: {gymId} not found'}), 404
+    
+    db.get_db().commit()
+    response = make_response({'message': f'Gym with id: {gymId} successfully deleted'})
+    response.status_code = 200
+
+    return response
+  except Exception as e: 
+    return jsonify({'error': str(e)}), 500
+
+# Get all gym request from the db
 @gyms.route('/gymRequests', methods=['GET'])
 def get_gym_request():
   try: 
