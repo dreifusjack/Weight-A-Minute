a48862028eb88a1ded26d282a16842875c8a9472
dreifusjack <dreifus.j@northeastern.edu>
2025-04-08 18:01:15 -0400

    implemtation of get all gym requests endpoint

--- api/backend/gyms/gyms_routes.py
+++ api/backend/gyms/gyms_routes.py
@@ -1,5 +1,29 @@
 from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from backend.db_connection import db
 
 gyms = Blueprint('gyms', __name__)
 
 # routes go here
+
+# route to get all gym requests 
+@gyms.route('/gymRequests', methods=['GET'])
+def get_gym_request():
+  try: 
+    query = '''
+    SELECT userId, gymDetails, requestDate 
+    FROM GymRequests
+    '''
+
+    cursor = db.get_db().get_cursor()
+    cursor.execute(query)
+
+    response = make_response(jsonify(cursor.fetchall()))
+    response.status_code = 200
+
+    return response
+  except Exception as e: 
+    return jsonify({'error': str(e)}), 500 
+
